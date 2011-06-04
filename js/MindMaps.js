8a9591f605f6bb5af0ba0ac3d86fe132ca8bfd91
David <david@david-thinkpad.(none)>
2011-06-04 10:52:42 +0200

    fixed console.xx calls on browsers that dont support it

--- js/MindMaps.js
+++ js/MindMaps.js
@@ -6,6 +6,32 @@ var mindmaps = mindmaps || {};
 
 // start up
 $(function() {
+	setupConsole();
+
 	var appController = new mindmaps.ApplicationController();
 	appController.go();
 });
+
+function setupConsole() {
+	var noOp = function() {
+	};
+
+	// provide console object and dummy functions if not built-in
+	console = console || {};
+	console.log = console.log || noOp;
+	console.info = console.info || noOp;
+	console.debug = console.debug || noOp;
+	console.warn = console.warn || noOp;
+	console.error = console.error || noOp;
+
+	// turn all console.xx calls into no-ops when in production mode
+	if (!mindmaps.DEBUG) {
+		console = {
+			log : noOp,
+			debug : noOp,
+			warn : noOp,
+			error : noOp,
+			info : noOp
+		};
+	}
+}
