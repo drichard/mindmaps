f1b2868e28130d426391c5eaaa2ef03e64809ac5
drichard <david@drichard.org>
2012-06-23 15:12:03 +0200

    Add debug file server

---
+++ server.js
@@ -0,0 +1,23 @@
+var sys = require('sys');
+var static = require('node-static');
+
+//
+// Create a node-static server to serve the current directory
+//
+var file = new(static.Server)('src', { cache: false, headers: {'X-Hello':'World!'} });
+
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response, function (err, res) {
+      if (err) {
+        sys.error("> Error serving " + request.url + " - " + err.message);
+        response.writeHead(err.status, err.headers);
+        response.end();
+      } else {
+        sys.puts("> " + request.url + " - " + res.message);
+      }
+    });
+  });
+}).listen(8080);
+
+sys.puts("> node-static is listening on http://127.0.0.1:8080");
